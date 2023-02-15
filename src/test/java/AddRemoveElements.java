c8c83350c93042a581843e210195be8e3617f84c
efremovdmitriy15 <efremovdmitriy15@gmail.com>
2023-02-15 20:20:53 +0300

    Initial commit

---
+++ src/test/java/AddRemoveElements.java
@@ -0,0 +1,26 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+import java.time.Duration;
+
+public class AddRemoveElements {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");//выставление системной переменной
+        WebDriver driver = new ChromeDriver();  //0. Открыть браузер
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+
+    }
+}
